d0b716c073edce05045ebccef54aff77daa29879
rickpeters <rick.peters@me.com>
2013-07-28 22:26:55 +0200

    initial version

---
+++ rubyboek/chap5.rb
@@ -0,0 +1,15 @@
+# pagina 25
+puts 'Hallo, wat is je naam?'
+name = gets.chomp
+puts 'je naam is ' + name + '? wat een mooie naam!'
+puts 'leuk om je te ontmoeten ' + name + '. :)'
+
+# pagina 26
+puts
+puts 'laten we een spelletje doen...'
+puts 'Geef me een getal: '
+getal = gets.chomp.to_i
+mooigetal = getal + 1
+puts 'je hebt me het getal ' + getal.to_s + ' gegeven, ik vind ' + mooigetal.to_s + ' eigenlijk mooier :)'
+puts
+
