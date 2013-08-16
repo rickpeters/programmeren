d3badb9b276bf3d959a6014036569899867a7506
rickpeters <rick.peters@me.com>
2013-08-16 19:02:57 +0200

    hfdst 13, Create your own classes, final version

--- rubyboek/chap13_1.rb
+++ rubyboek/chap13_1.rb
@@ -1,7 +1,20 @@
 # hfdst 13.1 maak je eigen classes
 class Die
+  def initialize
+    # gewoon een keer rollen, ook al kan ik een willekeurige waarde toekennen
+    roll
+  end
+
   def roll
     @number_showing = 1 + rand(6)
+  end
+
+  def cheat value
+    if value > 0 && value < 7
+      @number_showing = value
+    else
+      puts 'invalid value'
+    end
   end
 
   def showing
@@ -27,7 +40,7 @@ die.roll
 puts die.showing
 puts die.showing
 puts
-puts "die : #{Die.new.showing || 'nil'}"
-puts (Die.new.showing || 'nil')
+puts "die 1 : #{Die.new.showing || 'nil'}"
+puts "die 2 : #{Die.new.cheat(3) || 'nil'}"
 
 
